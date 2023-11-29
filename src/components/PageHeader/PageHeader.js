c9ac4ca2235ed622e4a05a2e4bfa79a42e4cf13b
Efrenoscar <efrenoscar6@gmail.com>
2023-11-29 09:43:53 -0400

    first commit

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,43 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+export default function PageHeader() {
+  return (
+    <div className="page-header header-filter">
+      <div className="squares square1" />
+      <div className="squares square2" />
+      <div className="squares square3" />
+      <div className="squares square4" />
+      <div className="squares square5" />
+      <div className="squares square6" />
+      <div className="squares square7" />
+      <Container>
+        <div className="content-center brand">
+          <h1 className="h1-seo">Start Up IoT</h1>
+          <h2>
+            Hacemos que los objetos de tu día a día sean más inteligentes. Bienvenido a la revolución del Internet de las Cosas.
+          </h2>
+        </div>
+      </Container>
+    </div>
+  );
+}
---
+++ src/views/Index.js
@@ -0,0 +1,461 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import PageHeader from "components/PageHeader/PageHeader.js";
+import Footer from "components/Footer/Footer.js";
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+export default function Index() {
+  // React.useEffect(() => {
+  //   document.body.classList.toggle("landing-page");
+  //   // Specify how to clean up after this effect:
+  //   return function cleanup() {
+  //     document.body.classList.toggle("landing-page");
+  //   };
+  // }, []);
+
+  React.useEffect(() => {
+    // document.body.classList.toggle("landing-page");
+        document.body.classList.toggle("index-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      // document.body.classList.toggle("landing-page");
+      document.body.classList.toggle("index-page");
+    };
+  }, []);
+  return (
+    <>
+      <IndexNavbar />
+      <div className="wrapper">
+        <PageHeader />
+      </div>
+      <div className="wrapper">
+        <section className="section section-lg">
+          <section className="section">
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path4.png")}
+            />
+            <Container>
+              <Row className="row-grid justify-content-between">
+                <Col className="mt-lg-5" md="5">
+                  <Row>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-trophy text-warning" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,237</CardTitle>
+                                <p />
+                                <p className="card-category">Awards</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats upper bg-default">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-coins text-white" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,653</CardTitle>
+                                <p />
+                                <p className="card-category">Commits</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-gift-2 text-info" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">593</CardTitle>
+                                <p />
+                                <p className="card-category">Presents</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-credit-card text-success" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">10,783</CardTitle>
+                                <p />
+                                <p className="card-category">Forks</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col md="6">
+                  <div className="pl-md-5">
+                    <br/>
+                    <h1>
+                    Desarrollo de Software
+                    </h1>
+                    <p>
+                    Somos una startup líder en el desarrollo de software de IoT. Creamos soluciones a medida 
+                    que permiten a los dispositivos y sistemas estar interconectados, transformando la forma 
+                    en que interactúas con el mundo que te rodea.
+                    </p>
+                    <br />
+                    <a
+                      className="font-weight-bold text-info mt-5"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Show all{" "}
+                      <i className="tim-icons icon-minimal-right text-info" />
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </section>
+        <section className="section section-lg">
+          
+          <Container>
+            <Row className="justify-content-center">
+              <Col lg="12">
+                <h1 className="text-center">¿Qué ofrecemos?</h1>
+                <Row className="row-grid justify-content-center">
+                  <Col lg="3">
+                    <div className="info">
+                      <div className="icon icon-primary">
+                        <i className="tim-icons icon-money-coins" />
+                      </div>
+                      <h4 className="info-title">Aplicaciones IoT</h4>
+                      <hr className="line-primary" />
+                      <p>
+                      Construimos aplicaciones de IoT que se ajustan a las necesidades de tu negocio
+                      </p>
+                    </div>
+                  </Col>
+                  <Col lg="3">
+                    <div className="info">
+                      <div className="icon icon-warning">
+                        <i className="tim-icons icon-chart-pie-36" />
+                      </div>
+                      <h4 className="info-title">Implementación IoT</h4>
+                      <hr className="line-warning" />
+                      <p>
+                      Te ayudamos a implementar el Internet de las cosas en tu negocio, 
+                      optimizando procesos y generando nuevas oportunidades.
+                      </p>
+                    </div>
+                  </Col>
+                  <Col lg="3">
+                    <div className="info">
+                      <div className="icon icon-success">
+                        <i className="tim-icons icon-single-02" />
+                      </div>
+                      <h4 className="info-title">Consultoría de IoT</h4>
+                      <hr className="line-success" />
+                      <p>
+                      Nuestro equipo de expertos te acompañará a través de todo el proceso de adopción de IoT.
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <section className="section section-lg section-safe">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path5.png")}
+          />
+          <Container>
+            <Row className="row-grid justify-content-between">
+              <Col md="5">
+                <img
+                  alt="..."
+                  className="img-fluid floating"
+                  src={require("assets/img/chester-wade.jpg")}
+                />
+                <Card className="card-stats bg-danger">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p">100%</CardTitle>
+                        <p className="card-category text-white">Safe</p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+                <Card className="card-stats bg-info">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p">573 K</CardTitle>
+                        <p className="card-category text-white">
+                          Satisfied customers
+                        </p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+                <Card className="card-stats bg-default">
+                  <CardBody>
+                    <div className="justify-content-center">
+                      <div className="numbers">
+                        <CardTitle tag="p">10 425</CardTitle>
+                        <p className="card-category text-white">Business</p>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="6">
+                <div className="px-md-5">
+                  <hr className="line-success" />
+                  <h3>Soluciones Inteligentes.</h3>
+                  <p>
+                  Nuestras soluciones de IoT pueden ayudarte a mejorar la eficiencia, 
+                  incrementar la productividad, impulsar la innovación y crear experiencias      
+                  increíbles para tus clientes.
+                  </p>
+                  <ul className="list-unstyled mt-5">
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-vector" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Sistemas de seguridad inteligentes.</h6>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-tap-02" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Iluminación inteligente</h6>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="d-flex align-items-center">
+                        <div className="icon icon-success mb-2">
+                          <i className="tim-icons icon-single-02" />
+                        </div>
+                        <div className="ml-3">
+                          <h6>Asistentes de voz</h6>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <section className="section section-lg section-coins">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path3.png")}
+          />
+          <Container>
+            <Row>
+              <Col md="4">
+                <hr className="line-info" />
+                <h1>
+                  Choose the coin{" "}
+                  <span className="text-info">that fits your needs</span>
+                </h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="4">
+                <Card className="card-coin card-plain">
+                  <CardHeader>
+                    <img
+                      alt="..."
+                      className="img-center img-fluid"
+                      src={require("assets/img/tuya.png")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      <Col className="text-center" md="12">
+                        <h4 className="text-uppercase">Light Coin</h4>
+                        <span>Plan</span>
+                        <hr className="line-primary" />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <ListGroup>
+                        <ListGroupItem>50 messages</ListGroupItem>
+                        <ListGroupItem>100 emails</ListGroupItem>
+                        <ListGroupItem>24/7 Support</ListGroupItem>
+                      </ListGroup>
+                    </Row>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button className="btn-simple" color="primary">
+                      Get plan
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-coin card-plain">
+                  <CardHeader>
+                    <img
+                      alt="..."
+                      className="img-center img-fluid"
+                      src={require("assets/img/tuya.png")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      <Col className="text-center" md="12">
+                        <h4 className="text-uppercase">Dark Coin</h4>
+                        <span>Plan</span>
+                        <hr className="line-success" />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <ListGroup>
+                        <ListGroupItem>150 messages</ListGroupItem>
+                        <ListGroupItem>1000 emails</ListGroupItem>
+                        <ListGroupItem>24/7 Support</ListGroupItem>
+                      </ListGroup>
+                    </Row>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button className="btn-simple" color="success">
+                      Get plan
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card className="card-coin card-plain">
+                  <CardHeader>
+                    <img
+                      alt="..."
+                      className="img-center img-fluid"
+                      src={require("assets/img/ttlock.png")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      <Col className="text-center" md="12">
+                        <h4 className="text-uppercase">Bright Coin</h4>
+                        <span>Plan</span>
+                        <hr className="line-info" />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <ListGroup>
+                        <ListGroupItem>350 messages</ListGroupItem>
+                        <ListGroupItem>10K emails</ListGroupItem>
+                        <ListGroupItem>24/7 Support</ListGroupItem>
+                      </ListGroup>
+                    </Row>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button className="btn-simple" color="info">
+                      Get plan
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* <div className="main">
+
+          <Basics />
+          <Navbars />
+          <Tabs />
+          <Pagination />
+          <Notifications />
+          <Typography />
+          <JavaScript />
+          <NucleoIcons />
+          <Signup />
+          <Examples />
+          <Download />
+        </div> */}
+        <Footer />
+      </div>
+    </>
+  );
+}
